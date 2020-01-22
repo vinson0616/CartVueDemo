89d0a73df6a847d10da2fa6673dfd859c3ecb666
vinson0616 <31696677+vinson0616@users.noreply.github.com>
2020-01-22 09:40:37 +0800

    create vue project

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 这个是用Vue2.6开发的一个简单的购物车组件
+使用VueCli4.1.2构建Vue项目结构
+1. 安装脚手架
+ npm install -g @vue/cli
+或
+yarn global add @vue/cli
+
+2. 创建项目
+vue create freemall
+
+3. 启动UI可视化界面
+vue ui
+
+下载代码后，首先执行以下命令：
+
+1.清理缓存 npm cache clean --force
+
+2.重新安装 npm install
---
+++ src/components/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div class="md-modal modal-msg md-modal-transition" :class="{'md-show': ShowModal}">
+            <div class="md-modal-inner">
+                <div class="md-top">
+                    <button class="md-close" @click="closeModal">关闭</button>
+                </div>
+                <div class="md-content">
+                    <div class="confirm-tips">
+                       <slot name="message"></slot>
+                    </div>
+                    <div class="btn-wrap">
+                        <slot name="btnGroup"></slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="md-overlay" v-if="ShowModal" @click="closeModal"></div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'modal',
+        props:['ShowModal'],
+        methods:{
+            closeModal(){
+                //调用父组件调用
+                this.$emit('close')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
